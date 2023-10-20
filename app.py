6f96e261e9a3fdda8c322dde5fedc2c399e121e3
LUZ MARINA PINZON <nunezjuanse@gmail.com>
2023-10-19 20:57:12 -0500

    version2

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+from flask import Flask, render_template, request
 import speech_recognition as sr
+import os
+import subprocess
+app = Flask(__name__)
 
-recognizer = sr.Recognizer()
-
-mic = sr.Microphone()
-
-with mic as source:
-    audio = recognizer.listen(source)
-
-text =  recognizer.recognize_google(audio, language = "ES")
-
-print("Has dicho: " + text)
+@app.route('/')
+def index():
+    recognizer = sr.Recognizer()
+    mic = sr.Microphone()
+    with mic as source:
+        audio = recognizer.listen(source)
+    text =  recognizer.recognize_google(audio, language = "ES")
+    if(text.lower() =="abrir chrome"):
+        resH = subprocess.Popen("start chrome", shell=True, stdout=subprocess.PIPE)
+        outH = resH.stdout.read()
+    print("Has dicho: " + text)
+    return render_template('index.html', text = text)
